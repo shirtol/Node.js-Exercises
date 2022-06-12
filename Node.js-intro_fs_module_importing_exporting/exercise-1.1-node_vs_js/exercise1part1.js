b03847f6c4d5c1ed2209f4aaf8d5268f2ceceb9f
shirtol <shirzahavi7@gmail.com>
2022-06-12 16:03:28 +0300

    Added exercise 1.1

---
+++ Node.js-intro_fs_module_importing_exporting/exercise-1.1-node_vs_js/exercise1part1.js
@@ -0,0 +1,15 @@
+/**
+ * ?3 things node.js has that the client side javascript doesn’t have:
+ * Can be executed outside the browser- because it is a server side platform.
+ * 1. The global command- because it doesn't run on the browser it doesn't have the window command.
+ * 2. The process command- In order to get data about the version, if we are on development state etc.
+ * 3. Buffer class - for performing operations on raw binary data. Use to save data in a certain way that give the client a fast experience?
+ * 4. assert- Will check the truths of an expression.
+ *
+ * ?3 things the client side javascript has that node.js doesn’t have:
+ * Can be executed on the browser- because it is related to the client side.
+ * Client side JS can manipulate DOM or add HTML within and Node.js doesn't have the capability to add HTML.
+ * 1. The window command- because Node.js doesn't work on the browser and JS is.
+ * 2. The document command
+ * 3. The Fetch command
+ */
