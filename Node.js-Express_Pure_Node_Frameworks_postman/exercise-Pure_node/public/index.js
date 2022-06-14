d526c71fcf6ce5eba70ee2ef39780c9b33447633
shirtol <shirzahavi7@gmail.com>
2022-06-14 23:26:25 +0300

    Deleted comments

--- Node.js-Express_Pure_Node_Frameworks_postman/exercise-Pure_node/public/index.js
+++ Node.js-Express_Pure_Node_Frameworks_postman/exercise-Pure_node/public/index.js
@@ -1,12 +1,1 @@
-// import http from "http";
-// const host = "localhost";
-// const port = 3000;
-
-// const requestListener = (req, res) => {};
-
-// const server = http.createServer(requestListener);
-// server.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}`);
-// });
-
 console.log("Hiiiiiiiiii");
