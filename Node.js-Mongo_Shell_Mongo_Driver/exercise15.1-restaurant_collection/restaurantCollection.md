36f00f3152c301470e7e461835ff7a4490f1d9d2
shirtol <shirzahavi7@gmail.com>
2022-06-19 20:39:19 +0300

    Added exercise 15.1

---
+++ Node.js-Mongo_Shell_Mongo_Driver/exercise15.1-restaurant_collection/restaurantCollection.md
@@ -0,0 +1,10 @@
+## Commends:
+
+-   create DB:
+    `use findMyRestaurant`
+
+-   create collection:
+    `db.createCollection("restaurants")`
+
+-   create documents:
+    `db.restaurants.insertMany(restaurant[])`
---
+++ Node.js-Mongo_Shell_Mongo_Driver/exercise15.1-restaurant_collection/restaurants.js
@@ -0,0 +1,122 @@
+const restaurants = [
+    {
+        name: "hamburg",
+        address: {
+            city: "Rehovot",
+            street: "Hamada",
+            coordinates: [-77.564, 40.677],
+        },
+        cuisineType: "Burgers",
+        kosher: true,
+        reviews: [
+            {
+                date: "11.2.22",
+                score: 5,
+            },
+            {
+                date: "12.2.22",
+                score: 4,
+            },
+            {
+                date: "13.2.22",
+                score: 5,
+            },
+        ],
+    },
+    {
+        name: "Nishi",
+        address: {
+            city: "Rehovot",
+            street: "Frank",
+            coordinates: [-72.164, 20.677],
+        },
+        cuisineType: "Asia",
+        kosher: true,
+        reviews: [
+            {
+                date: "31.2.22",
+                score: 5,
+            },
+            {
+                date: "10.2.22",
+                score: 2,
+            },
+            {
+                date: "30.2.22",
+                score: 3,
+            },
+        ],
+    },
+    {
+        name: "Cafe Cafe",
+        address: {
+            city: "Hadera",
+            street: "Hamada",
+            coordinates: [-17.564, -40.677],
+        },
+        cuisineType: "Cafe",
+        kosher: true,
+        reviews: [
+            {
+                date: "21.2.22",
+                score: 5,
+            },
+            {
+                date: "22.2.22",
+                score: 4,
+            },
+            {
+                date: "23.2.22",
+                score: 5,
+            },
+        ],
+    },
+    {
+        name: "japanika",
+        address: {
+            city: "Rehovot",
+            street: "Haim Pekris",
+            coordinates: [-74.564, 40.797],
+        },
+        cuisineType: "Asia",
+        kosher: true,
+        reviews: [
+            {
+                date: "18.2.22",
+                score: 5,
+            },
+            {
+                date: "19.2.22",
+                score: 1,
+            },
+            {
+                date: "20.2.22",
+                score: 3,
+            },
+        ],
+    },
+    {
+        name: "a",
+        address: {
+            city: "Tel Aviv",
+            street: "Dizengoff",
+            coordinates: [-47.564, 20.677],
+        },
+        cuisineType: "Asia",
+        kosher: false,
+        reviews: [
+            {
+                date: "14.2.22",
+                score: 5,
+            },
+            {
+                date: "15.2.22",
+                score: 5,
+            },
+            {
+                date: "16.2.22",
+                score: 5,
+            },
+        ],
+    },
+];
